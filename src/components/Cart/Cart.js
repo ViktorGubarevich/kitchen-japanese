6dcb2fdd4eb0c6dcce963b9b56c39a0749f8c8a4
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2023-01-02 20:04:19 +0300

    Added logic for REMOVE_ITEM.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,12 +7,16 @@ import CartItem from "./CartItem";
 const Cart = ({ onHideCart }) => {
   const cartContext = useContext(CartContext);
 
-  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+  const totalAmount = `$${Math.abs(cartContext.totalAmount).toFixed(2)}`;
   const hasItems = cartContext.items.length > 0;
 
-  const addCartItemHandler = (item) => {};
+  const addCartItemHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
-  const removeCartItemHandler = (id) => {};
+  const removeCartItemHandler = (id) => {
+    cartContext.removeItem(id);
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
